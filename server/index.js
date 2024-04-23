4c6dcec4dc96871b00aaceaae570dd34cfe9c494
nirmalsolanki30 <solanki.nirmal847@gmail.com>
2024-04-23 10:39:41 +0530

    created Product and ProductStat Schema and wrote code to send all products with their stats

--- server/index.js
+++ server/index.js
@@ -12,8 +12,9 @@ import salesroutes from "./routes/sales.js";
 
 /* data imports */
 import User from "./models/User.js";
-import { dataUser } from "./data/index.js";
-
+import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
 
 /* Configuration */
 dotenv.config();
@@ -41,6 +42,9 @@ mongoose.connect(process.env.MONGO_URL, {
 }).then(()=>{
     app.listen(PORT,()=>console.log(`Server Port: ${PORT}`));
     // Add data one by one
+    // Product.insertMany(dataProduct);
+    // ProductStat.insertMany(dataProductStat);
+
     // User.insertMany(dataUser);
 })
 .catch((error) => console.log(`${error} did not connect`));
