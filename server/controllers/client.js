da37f1557a6f1536ff4d85ba81f77b3804d1e04c
Krishna-Morker <morkerkrishna83@gmail.com>
2024-04-25 00:21:15 +0530

    Completing Customer Page

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,6 +1,6 @@
 import Product from '../models/Product.js'
 import ProductStat from '../models/ProductStat.js'
-
+import User from "../models/User.js";
 export const getProducts = async (req,res) => {
     try{
         const products = await Product.find();
@@ -22,3 +22,13 @@ export const getProducts = async (req,res) => {
         res.status(404).json({message: err.message});
     }
 }
+
+
+export const getCustomers = async (req, res) => {
+    try {
+      const customers = await User.find({ role: "user" }).select("-password");
+      res.status(200).json(customers);
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+  };
