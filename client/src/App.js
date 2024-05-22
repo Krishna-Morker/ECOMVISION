22aa54af80c624566e0cf4f00919829dc6f757cf
Krishna-Morker <morkerkrishna83@gmail.com>
2024-05-22 16:04:08 +0530

    Completing Monthly page

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import Transactions from 'scenes/transactions';
 import Geography from "scenes/geography";
 import Overview from "scenes/overview";
 import Daily from 'scenes/daily';
+import Monthly from 'scenes/monthly';
 function App() {
   const mode=useSelector((state)=>state.global.mode)
   const theme= useMemo(()=>createTheme(themeSettings(mode)),[mode])
@@ -30,6 +31,7 @@ function App() {
                  <Route path="/geography" element={<Geography />} />
                  <Route path="/overview" element={<Overview />} />
                  <Route path="/daily" element={<Daily />} />
+                 <Route path="/monthly" element={<Monthly />} />
               </Route>
           </Routes>
         </ThemeProvider>
